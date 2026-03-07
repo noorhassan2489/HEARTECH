0c6ea4f2a68789e9c5074089ddc6663080bd8290
Noor Hassan <noorhassan@Noors-MacBook-Pro.local>
2026-03-07 21:11:04 +0500

    Auto-sync: file changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MaterialApp(home: LoginScreen()));
+}
+
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
